741072605cca10b4109f4e3dfb41765f32fd91d2
Marcelo Victor <lvictor462@gmail.com>
2023-09-04 10:25:19 -0300

    att branch7

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
 
 import TelaA from './src/views/TelaA';
 import TelaB from './src/views/TelaB';
 import TelaC from './src/views/TelaC';
 
+const Stack = createNativeStackNavigator()
+
 export default function App() {
+  
   return (
-    <SafeAreaView style={styles.container}>
-      <TelaA />
-      <TelaB />
-      <TelaC />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="TelaA" component={TelaA} />
+        <Stack.Screen name="TelaB" component={TelaB} />
+        <Stack.Screen name="TelaC" component={TelaC} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
